896eb21fb09af759f9b5f2e33a67d4fe478875f2
Griselda Sanchez <googlerax@hotmail.com>
2022-08-09 22:07:07 -0300

    first commit

---
+++ ejercicio-1/src/com/example/DataType.java
@@ -0,0 +1,54 @@
+package com.example;
+
+public class DataType {
+
+
+    public static void main(String[] args) {
+
+
+        //-----------------tipos de datos primitivos---------------------
+        byte vari1 = 4;
+        short vari2 = 10;
+        int vari3 = 30;
+        long vari4 = 100;
+
+        //decimales:
+
+        float vari5 = 4.5f;
+        double vari6 = 4.25d;
+
+
+        //booleanos:
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //caracteres:
+        char caracter = 'x';   // entrecomillado simple.
+
+
+        //-----------------clase:----------------------------
+
+        //cadena de texto:
+        String texto = "hola mundo";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
